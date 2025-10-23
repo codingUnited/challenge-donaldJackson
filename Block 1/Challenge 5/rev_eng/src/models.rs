4eee66a3bdc38a2d78cd30614c52784139a30bb0
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-10-22 23:36:14 -0400

    feat(models): Book struct to host the isbn, title, author, and pages.

---
+++ Block 1/Challenge 5/rev_eng/src/models.rs
@@ -0,0 +1,18 @@
+use crate::utils;
+
+
+// This replaces the C++ Book class.
+pub struct Book{
+    pub isbn: String,
+    pub title: String,
+    pub author: String,
+    pub pages: Vec<String>,
+}
+
+pub struct User{
+    // Nothing Here Yet.
+}
+
+pub struct BookReadingSession{
+    // Nothing Here Yet.
+}
