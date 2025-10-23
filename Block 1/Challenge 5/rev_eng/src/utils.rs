02c4b49c3e0b394825a9295811a345ad02310880
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-10-22 22:30:09 -0400

    feat(utils): Implemented improved versions of the buggy.cpp utility functions. Added Panic!() statements for if I make an error later when using the functions made. This change was made directly after seeing a tiktok about the panic feature.  With this the `utils.rs` module is completed

--- Block 1/Challenge 5/rev_eng/src/utils.rs
+++ Block 1/Challenge 5/rev_eng/src/utils.rs
@@ -5,8 +5,10 @@ use chrono::prelude::*;
 // If the user enters an invalid input, it will prompt the user to enter a valid input.
 pub fn read_int(low: i32, high: i32) -> i32 {
     if low > high {
-        println!("Invalid range");
-        return -1; // Return -1 to indicate an error.
+        // changed to a panic, since this is a programming error, not a user input error.
+        panic!("Invalid range: low ({}) is greater 
+                than high ({})", low, high);
+
     }
 
     loop {
@@ -16,7 +18,7 @@ pub fn read_int(low: i32, high: i32) -> i32 {
         let mut choice = String::new();
 
         if stdin().read_line(&mut choice).is_err() {
-            println!("Failed to read line");
+            println!("Failed to read line. Input again.");
             continue;
         }
 
@@ -27,7 +29,7 @@ pub fn read_int(low: i32, high: i32) -> i32 {
             //  Match can fail to parse, if for example the user uses a non-integer value.
             //  If that happens print an error message and continue the loop.
             _ => {
-                println!("Error: invalid number...Try again");
+                println!("Error: Invalid number...Try again");
             }
         }
     }
@@ -36,6 +38,10 @@ pub fn read_int(low: i32, high: i32) -> i32 {
 pub fn show_read_menu(choices: &[&str]) -> i32 {
     print!("\nMenu\n");
     stdout().flush().unwrap(); // Menu will appear before options
+
+    if choices.is_empty(){ // Sanity check for empty choices array, programmign error not user input error.
+        panic!("Called with an empty choices array.")
+    }
 
     // Enumerate then add one for display purposes.
     for (index, choice) in choices.iter().enumerate(){
@@ -54,4 +60,34 @@ pub fn get_current_time_and_date() -> String {
 
     // Format the string to match buggy.cpp format
     current_time.format("%Y-%m-%d %H:%M:%S").to_string()
+}
+
+// This is to fix issues with buggy.cpp where cin >> str 
+// only reads the first word in a title/string.\
+// technically this is not needed for the challenge as rust's
+// read_line reads the whole line by default.
+// This handles input sanitization so there are no empty strings inputted.
+pub fn read_input_string(prompt: &str) -> String {
+    loop {
+        print!("{}", prompt);
+        stdout().flush().unwrap();
+
+        let mut input = String::new();
+
+        // if read_line has an error, print an error message and continue loop.'
+        if stdin().read_line(&mut input).is_err() {
+            println!("Error: failed to read line. Try again.");
+            continue;
+        }
+
+        // otherwise we trim the input and return it.
+
+        let trimmed_input = input.trim().to_string();
+        if trimmed.is_empty() {
+            println!("Error: Input should not be empty. Try again.");
+            continue;
+        }
+
+        return trimmed_input; // return the trimmed input string when valid.
+    }
 }
