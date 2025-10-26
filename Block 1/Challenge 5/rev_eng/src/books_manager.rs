6a2dce851a2a1a3a5bc7cdf765ddb840c09003e1
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-10-26 18:55:59 -0400

    feat(books_managers.rs): Implemented the load_database function with some dummy data.

---
+++ Block 1/Challenge 5/rev_eng/src/books_manager.rs
@@ -0,0 +1,49 @@
+// crates and collections.
+use std::collections::HashMap;
+use crate::models::Book;
+
+
+// Fixes the problem with duplicate ISBNs
+// while allowing for fast (O(1)) lookups if you have the ISBN.
+
+pub struct BooksManager {
+    books: HashMap<String, Book>,
+}
+
+impl BooksManager {
+    pub fn new() -> Self {
+        Self {
+            books: HashMap::new(),
+        }
+    }
+
+    pub fn load_database(&mut self) {
+        self.books.clear(); // Clear existing data
+
+        // sample books!
+        let book1 = Book::new(
+            "The Rust Programming Language".to_string(),
+            "Steve Klabnik and Carol Nichols".to_string(),
+            "9781718503106".to_string(),
+            vec![]
+        ); 
+
+        let book2 = Book::new(
+            "C++ for Dummies".to_string(),
+            "Stephen R. Davis".to_string(),
+            "9780470317266".to_string(),
+            vec![]
+        );
+
+        let book3 = Book::new(
+            "Continuous Deployment: Enable Faster Feedback, Safer Releases, and More Reliable Software".to_string(),
+            "Valentina Servile".to_string(),
+            "9781098146726".to_string(),
+            vec![]
+        );
+
+        self.add_book(book1);
+        self.add_book(book2);
+        self.add_book(book3);   
+    }
+}
